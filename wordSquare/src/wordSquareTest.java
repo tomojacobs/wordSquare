4a85d07108095f0fa743f5af5722aa8eaa913b01
Tom <tom.o.jacobs@hotmail.com>
2021-04-16 19:59:45 +0100

    Dictionary read, word validation, next to loop through valid words to build square.

---
+++ wordSquare/src/wordSquareTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class wordSquareTest {
+
+    @BeforeAll
+    void makeGame(){
+        //wordSquare wordSquare = new wordSquare("4 eeeeddoonnnsssrv");
+    }
+
+    @Test
+    void getBoardSize() {
+        //assertEquals(4, wordSquare.getWordSquareSize());
+    }
+}
